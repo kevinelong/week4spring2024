87aca929864fefc098cd5f7eb4d113ef5a2de92d
kevinelong <kevinelong@gmail.com>
2024-05-01 11:03:20 -0700

    demo

--- exercises/Dates/exercisesDatesOffBy1Website/index.js
+++ exercises/Dates/exercisesDatesOffBy1Website/index.js
@@ -3,15 +3,22 @@ function handleClick(e){
     const dateInput = document.getElementById("dateInput");
     const outputParagraph = document.getElementById("outputParagraph");
 
-    let rawText = dateInput.value;
-    let dateObject = new Date(rawText);
-    let outputText = dateObject.toString();
+    const rawText = dateInput.value;
+    const dateObject = new Date(rawText);
+    const outputText = dateObject.toString();
     outputParagraph.innerHTML = rawText + "<br><br>" + outputText;
 }
 
 function handleLoaded(e){
     const displayButton = document.getElementById("displayButton");
-    
+
     displayButton.addEventListener("click", handleClick);
 }
 document.addEventListener("DOMContentLoaded", handleLoaded);
+
+//NON-FUNCTION BLOCK SCOPE EXAMPLE "IF" see also "for(){}"
+const e = 456;
+if(true){ //BEGIN BLOCK
+    const e = 123;
+} //END BLOCK
+console.log(e); //456
